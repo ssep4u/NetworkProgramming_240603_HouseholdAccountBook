017bfc1827763105c0b84c81aae70151dccdb4e7
임t <ssep4u@gmail.com>
2024-06-10 11:42:20 +0900

    chart_test 완료(labels, data, backgroundColor)

--- chart_test/views.py
+++ chart_test/views.py
@@ -1,5 +1,21 @@
+import json
+
 from django.shortcuts import render
 
+
 def show_chart(request):
-    context = {}
+    # open(파일경로, 모드(r=읽기, w=쓰기, a=내용추가, b=바이너리파일, x=파일생성), 인코딩)
+    with open('data/data.json', 'r', encoding='utf-8') as file:
+        json_data = json.load(file)
+
+    # 데이터 분해하기
+    name = [item['name'] for item in json_data]
+    price = [item['price'] for item in json_data]
+    ctgColor = [item['ctgColor'] for item in json_data]
+
+    context = {
+        'name': name,
+        'price': price,
+        'ctgColor': ctgColor
+    }
     return render(request, 'chart_test/chart_view.html', context=context)
