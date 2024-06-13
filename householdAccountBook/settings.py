4615b7f2935b6b2503c3631311c9dcde3a98ec79
임t <ssep4u@gmail.com>
2024-06-13 15:32:41 +0900

    base.html {% block title %}{% block content %}

--- householdAccountBook/settings.py
+++ householdAccountBook/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'householdAccountBook.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
